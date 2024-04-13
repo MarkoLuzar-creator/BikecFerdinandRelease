bc16d87e41215bcfcf1ed75b9fec72f8e6c98a7d
MarkoLužar <Marko.luzar2@gmail.com>
2024-04-13 16:43:16 +0200

    alfa beta release

--- koda/objects/Arena.cpp
+++ koda/objects/Arena.cpp
@@ -2,26 +2,29 @@
 
 int Arena::Update(int playerPositionX, int playerPositionY, int playerSizeX, int playerSizeY)
 {
-	int cx, cy;
-	CameraHandler::GetInstance().GetPosition(cx, cy);
-	if (RigidBody::Collide(x, y, w, h, playerPositionX, playerPositionY, playerSizeX, playerSizeY))
+	if (!openDoor)
 	{
-		x = rand() % (1920);
-		y = rand() % (1080);
-		if (count == 1)
+		int cx, cy;
+		CameraHandler::GetInstance().GetPosition(cx, cy);
+		if (RigidBody::Collide(x, y, w, h, playerPositionX, playerPositionY, playerSizeX, playerSizeY))
 		{
-			openDoor = true;
-			stop = true;
-			return 50;
+			x = rand() % (1920);
+			y = rand() % (1080);
+			if (count == 1)
+			{
+				openDoor = true;
+				stop = true;
+				return 50;
+			}
+			count--;
+			return 25;
 		}
-		count--;
-		return 25;
-	}
 
-	if (RigidBody::GetDistance(x, y, playerPositionX, playerPositionY) < 500)
-	{
-		TextureHandler::GetInstance().DrawTexture(texture, x - cx,
-		y - cy, w, h, SDL_FLIP_NONE, scale);
+		if (RigidBody::GetDistance(x, y, playerPositionX, playerPositionY) < 500)
+		{
+			TextureHandler::GetInstance().DrawTexture(texture, x - cx,
+				y - cy, w, h, SDL_FLIP_NONE, scale);
+		}
 	}
 
 	return 0;
